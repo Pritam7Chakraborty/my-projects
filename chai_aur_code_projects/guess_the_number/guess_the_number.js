bdaf45b6fc368ab9eb6c80777f52bb87f148fee4
Pritam7Chakraborty <cpritam870@gmail.com>
2024-12-06 23:03:38 +0530

    worked more on guess the number

--- chai_aur_code_projects/guess_the_number/guess_the_number.js
+++ chai_aur_code_projects/guess_the_number/guess_the_number.js
@@ -23,12 +23,51 @@ if (playgame == true) {
   });
 }
 
-function validateGuess(guess) {}
+function validateGuess(guess) {
+  if(isNaN(guess)){
+    alert(`Please enter a Valid Number:`);
+  }
+  else if(guess<1 || guess>100){
+    alert(`Please enter a Valid Number:`);
+  }
+  else{
+    prevGuess.push(guess);
+    if(numGuess === 11){
+      displayGuess(guess);
+      displayMessage(`Game over. Random Number was ${randomNumber}`);
+      endgame();
+    }
+    else{
+      displayGuess(guess);
+      checkGuess(guess);
+    }
+  }
+}
 
-function checkGuess(guess) {}
+function checkGuess(guess) {
+   if(guess === randomNumber){
+      displayMessage(`you guessed it right`);
+      endgame();
+   }
+   else if(guess<randomNumber){
+    displayMessage(`Number is too low`)
+   }
+   else if(guess>randomNumber){
+    displayMessage(`Number is too high`)
+   }
+}
+
+function displayGuess(guess){
+  userInput.value='';
+  guesslot.innerHTML += `${guess}`;
+  numGuess++;
+  remainingGuess.innerHTML = `${11-numGuess}`;
+}
 
 function displayMessage(Message) {}
 
 function endgame() {}
 
 function newGame() {}
+
+
