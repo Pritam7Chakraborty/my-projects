168a7c491f10b0b4770c5dd060ff62051e4e2feb
Pritam7Chakraborty <cpritam870@gmail.com>
2025-06-30 11:46:27 +0530

    Spring WebSocket Chat Demo - By Pritam

---
+++ chat/src/main/java/com/learningComponents/chat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.learningComponents.chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ ems-backend/src/main/java/net/crudbasics/ems_backend/config/WebSocketConfig.java
@@ -0,0 +1,9 @@
+package net.crudbasics.ems_backend.config;
+
+import jdk.jfr.Enabled;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig {
+}
