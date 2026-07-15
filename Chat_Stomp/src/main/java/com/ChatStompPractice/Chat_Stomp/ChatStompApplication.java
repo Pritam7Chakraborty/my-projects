5fc38bef18ed43d8e722917dff676e6b83258812
Pritam Chakraborty <your_actual_email@gmail.com>
2026-07-15 23:19:53 +0530

    working on thumbnail day1

---
+++ Chat_Stomp/src/main/java/com/ChatStompPractice/Chat_Stomp/ChatStompApplication.java
@@ -0,0 +1,13 @@
+package com.ChatStompPractice.Chat_Stomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatStompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatStompApplication.class, args);
+	}
+
+}
---
+++ Chat_Stomp/src/test/java/com/ChatStompPractice/Chat_Stomp/ChatStompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChatStompPractice.Chat_Stomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatStompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
